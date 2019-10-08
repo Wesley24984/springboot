6920f58ba0e3cf0cb42bbd41bc2d981756838ce6
liwei <lw9121@163.com>
2019-10-08 22:34:50 +0800

    update package

--- src/main/java/com/liwei/ServletInitializer.java
+++ src/main/java/com/liwei/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.liwei;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/test/java/com/liwei/DemoApplicationTests.java
+++ src/test/java/com/liwei/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.liwei;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
